23d572402b6f4ddcb0f0e58a5b0cffa51eb81f2e
Herosuit3 <teerawat2441@gmail.com>
2025-12-28 17:38:05 +0700

    set up async db and สร้าง table ด้วย class, connection กับ db

---
+++ app/core/db.py
@@ -0,0 +1,23 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from dotenv import load_dotenv
+from sqlalchemy.orm import DeclarativeBase
+
+load_dotenv() # โหลด .env จาก root folder โดยอัตโนมัติ
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# 1. สร้าง Async Engine
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+# 2. สร้าง SessionMaker สำหรับสร้าง session ใหม่ในแต่ละ request
+async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+# 3. Base Class สำหรับ Model
+class Base(DeclarativeBase):
+    pass
+
+# ฟังก์ชันสำหรับ Dependency Injection ใน API Routes
+async def get_db():
+    async with async_session() as session:
+        yield session
