1d479eb3464948d6e38e5d8bd9d09b60cbfc483d
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-02 06:50:24 +0700

    init asset + asset schema

---
+++ app/schemas/asset.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class AssetCreate(BaseModel):
+    name: str
+    project_id: int
+    credential_id: Optional[int] = None
+    description: str
+    target: str
+    type: str
+
+class AssetResponse(BaseModel):
+    name: str
+    project_id: int
+    credential_id: Optional[int] = None
+    description: str
+    target: str
+    type: str
+
+    class Config:
+        orm_mode = True
