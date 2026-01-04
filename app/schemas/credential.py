d79d50f3f33e466d87797c91baf171245ec365d4
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-05 02:53:31 +0700

    แก้ type ของ asset_id จาก str -> int

--- app/schemas/credential.py
+++ app/schemas/credential.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
 
 class CredentialCreate(BaseModel):
-    asset_id: str
+    asset_id: int
     username: str
     password: str
 
 class CredentialResponse(BaseModel):
-    asset_id: str
+    asset_id: int
     username: str
     password: str
 
