43df453fb8c3be4dcca943af268c9da760a0fd1a
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-05 01:50:37 +0700

    สร้าง schema ของ credential

--- app/schemas/credential.py
+++ app/schemas/credential.py
@@ -1,23 +1,14 @@
 from pydantic import BaseModel
-from typing import Optional
-from datetime import datetime
 
-class AssetCreate(BaseModel):
-    name: str
-    project_id: int
-    credential_id: Optional[int] = None
-    description: str
-    target: str
-    type: str
+class CredentialCreate(BaseModel):
+    asset_id: str
+    username: str
+    password: str
 
-class AssetResponse(BaseModel):
-    name: str
-    project_id: int
-    credential_id: Optional[int] = None
-    description: str
-    target: str
-    type: str
-    updated_at: datetime
+class CredentialResponse(BaseModel):
+    asset_id: str
+    username: str
+    password: str
 
     class Config:
         orm_mode = True
