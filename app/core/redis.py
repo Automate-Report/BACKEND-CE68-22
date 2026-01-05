f6df8a8dfa4d5762e8e3f3ce8b5d6d526de14d91
Herosuit3 <teerawat2441@gmail.com>
2026-01-05 14:05:23 +0700

    redis change port to 5678

--- app/core/redis.py
+++ app/core/redis.py
@@ -2,9 +2,9 @@ import redis
 
 redis_client = redis.Redis(
     host="localhost",
-    port=6379,
+    port=5678,
     db=0,
     decode_responses=True
 )
 
-#docker run -d --name redis -p 6379:6379 redis
+#docker run -d --name redis -p 5678  redis:6379 redis
