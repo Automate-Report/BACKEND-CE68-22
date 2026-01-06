133da3429b7f59280ab5c162dc8721b710334d51
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-06 20:28:35 +0700

    แก้ไข schema ของ asset

--- app/schemas/asset.py
+++ app/schemas/asset.py
@@ -5,7 +5,6 @@ from datetime import datetime
 class AssetCreate(BaseModel):
     name: str
     project_id: int
-    credential_id: Optional[int] = None
     description: Optional[str] = None
     target: str
     type: str
@@ -14,7 +13,6 @@ class AssetResponse(BaseModel):
     id: int
     name: str
     project_id: int
-    credential_id: Optional[int] = None
     description: Optional[str] = None
     target: str
     type: str
