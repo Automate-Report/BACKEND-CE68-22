cd09347736f5769a6071b8fdf07847c4d56129ef
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-20 23:52:45 +0700

    ปิด life span

--- app/main.py
+++ app/main.py
@@ -53,7 +53,7 @@ app = FastAPI(
     title="CE68-22 Backend API",
     description="API for Project (Master-Agent Architecture)",
     version="1.0.0",
-    lifespan=lifespan
+    # lifespan=lifespan
 
 )
 
