991f537adb6aad1f2f669d3b5dc4bd179ee92246
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-23 21:15:28 +0700

    เพิ่มส่วนของ worker ใน redis ที่ db=1

--- app/core/redis.py
+++ app/core/redis.py
@@ -6,5 +6,16 @@ redis_client = redis.Redis(
     db=0,
     decode_responses=True
 )
+redis_worker = redis.Redis(
+    host="10.20.20.108",
+    post=5678,
+    db=1,
+    decode_responses=True
+)
+
+SECHEDULE_KEY = "system:schedule" #เก็บงานรอเวลา
+QUEUE_KEY = "system:queue" #เก็บงานที่พร้อมทำ
+
+
 
 #docker run -d --name redis -p 5678  redis:6379 redis
