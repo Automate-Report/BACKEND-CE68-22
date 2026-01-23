8ccdbc11fe427d4d82d07ddee8d18d009951bd33
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-24 01:57:59 +0700

    ใช้ redis แบบ async และแยก backlist กับ job

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,21 +1,32 @@
-import redis
+import redis.asyncio as redis
 
-redis_client = redis.Redis(
-    host="10.20.20.108",
-    port=5678,
-    db=0,
-    decode_responses=True
-)
-redis_worker = redis.Redis(
-    host="10.20.20.108",
-    port=5678,
-    db=1,
-    decode_responses=True
+pool_blacklist = redis.ConnectionPool.from_url(
+    url="redis://10.20.20.108:5678/0",
+    decode_response=True
 )
 
-SECHEDULE_KEY = "system:schedule" #เก็บงานรอเวลา
+pool_jobs = redis.ConnectionPool.from_url(
+    url="redis://10.20.20.108:5678/1",
+    decode_response=True
+)
+
+
+# redis_client = redis.Redis(
+#     host="10.20.20.108",
+#     port=5678,
+#     db=0,
+#     decode_responses=True
+# )
+# redis_worker = redis.Redis(
+#     host="10.20.20.108",
+#     port=5678,
+#     db=1,
+#     decode_responses=True
+# )
+
+redis_client = redis.Redis(connection_pool=pool_blacklist)
+redis_jobs = redis.Redis(connection_pool=pool_jobs)
+
 QUEUE_KEY = "system:queue" #เก็บงานที่พร้อมทำ
-
-
 
 #docker run -d --name redis -p 5678  redis:6379 redis
