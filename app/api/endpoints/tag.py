97f924828f92bc5af348823ad7eb5f798894c077
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-26 21:24:01 +0700

    แก้ไขเรื่องรูปแบบการส่งข้อมูล

--- app/api/endpoints/tag.py
+++ app/api/endpoints/tag.py
@@ -10,9 +10,25 @@ router = APIRouter()
 
 # GET /projects/ : ดึงโปรเจกต์ทั้งหมดของ user นั้น
 @router.get("/all/{user_id}", response_model=List[TagsResponse])
-async def get_all_tags(user_id: str):
-    tags = tag_service.get_all_tags(user_id)
+async def get_all_tags_by_user_id(user_id: str):
+    tags = tag_service.get_all_tags_by_user_id(user_id)
     return tags
+
+@router.get("/project/{project_id}", response_model=List[TagsResponse])
+async def get_all_tags_by_project_id(project_id: int):
+    tag_ids = project_tag_service.get_all_tag_ids(project_id=project_id)
+    tags = []
+    for id in tag_ids:
+        t = tag_service.get_tag_by_id(id)
+        tags.append(t)
+    return tags
+
+@router.get("/{tag_id}", response_model=TagsResponse)
+async def get_tag_by_id(tag_id: int):
+    tag = tag_service.get_tag_by_id(tag_id)
+    if not tag:
+        raise HTTPException(status_code=404, detail="Tag not found")
+    return tag
 
 @router.post("/", response_model=TagsResponse)
 async def create_tag(tag_in: TagCreate):
