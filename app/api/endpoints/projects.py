7916bf81757c6d42d0a8b84d26f823a2948e8f3e
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-26 19:41:38 +0700

    ลบ relation กับ tag ก่อนลบ project

--- app/api/endpoints/projects.py
+++ app/api/endpoints/projects.py
@@ -91,6 +91,10 @@ async def delete_project(project_id: int):
     success = project_service.delete_project(
         project_id=project_id
     )
+    if delete_relation is True:
+        success = project_service.delete_project(
+            project_id=project_id
+        )
     if not success:
         raise HTTPException(status_code=404, detail="Project not found")
     return {"detail": "Project deleted successfully"}
