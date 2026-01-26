f827ef3bed84ef1d3212993f205729b4cb3efc64
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-26 21:42:28 +0700

    เพิ่มลบ relation project tag

--- app/api/endpoints/project_tags.py
+++ app/api/endpoints/project_tags.py
@@ -10,3 +10,14 @@ router = APIRouter()
 async def get_all_tags(project_id: str):
     tags = project_tag_service.get_all_tag_ids(project_id)
     return tags
+
+@router.delete("/projects/{project_id}/tags/{tag_id}")
+async def delete_relation_project_tag(project_id:int, tag_id:int):
+    success = project_tag_service.delete_relation(
+        project_id=project_id,
+        tag_id=tag_id
+    )
+
+    if not success:
+        raise HTTPException(status_code=404, detail="Project or Tag not found")
+    return {"detail": "Project deleted successfully"}
--- app/services/project_tag.py
+++ app/services/project_tag.py
@@ -76,11 +76,20 @@ class ProjectTagService:
                 return True
         return False
     
-    def delete_project(self, project_id: int) -> bool:
+    def delete_by_project_id(self, project_id: int) -> bool:
         """Service: ลบโปรเจกต์"""
         project_tags = self._read_json()
         for i, proj in enumerate(project_tags):
             if proj["project_id"] == project_id:
+                del project_tags[i]
+                self._save_json(project_tags)
+                return True
+        return False
+    
+    def delete_relation(self, project_id: int, tag_id: int) -> bool:
+        project_tags = self._read_json()
+        for i, proj in enumerate(project_tags):
+            if proj["project_id"] == project_id and proj["tag_id"] == tag_id:
                 del project_tags[i]
                 self._save_json(project_tags)
                 return True
