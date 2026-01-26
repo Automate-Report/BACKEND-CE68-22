0a2f79b8e653a7eee31977783688de34c9d53a68
Herosuit3 <teerawat2441@gmail.com>
2026-01-27 00:00:24 +0700

    make change to model of yables in db

--- app/models/jobs.py
+++ app/models/jobs.py
@@ -15,7 +15,6 @@ class Job(Base):
     __tablename__ = "jobs"
     id:Mapped[int] = mapped_column(sa.Integer, autoincrement=True, primary_key=True)#=======================ULID
     schedule_id:Mapped[int] = mapped_column(sa.ForeignKey("schedules.id")) #======================================FK ULID
-    worker_id:Mapped[int] = mapped_column(sa.ForeignKey("workers.id")) #======================================FK ULID
     status:Mapped[JobStatus] = mapped_column(sa.Enum(JobStatus), default=JobStatus.PENDING)
     started_at:Mapped[Optional[datetime.datetime]] = mapped_column(sa.DateTime(timezone=True), default=None)
     finished_at:Mapped[Optional[datetime.datetime]] = mapped_column(sa.DateTime(timezone=True), default=None)
