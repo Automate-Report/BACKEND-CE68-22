eb2ad31a342460705144cb1085f4f0a03fa9702d
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-26 19:58:56 +0700

    แก้ไข logic การลบ

--- app/api/endpoints/projects.py
+++ app/api/endpoints/projects.py
@@ -72,10 +72,11 @@ async def delete_project(project_id: int):
     delete_relation = project_tag_service.delete_project_tags(
         project_id=project_id
     )
-    if delete_relation is True:
-        success = project_service.delete_project(
-            project_id=project_id
-        )
+
+
+    success = project_service.delete_project(
+        project_id=project_id
+    )
     if not success:
         raise HTTPException(status_code=404, detail="Project not found")
     return {"detail": "Project deleted successfully"}
