d74fdbf60ce4a356b4b31405a808b4e79285c9d5
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-29 23:56:08 +0700

    แก้ไขชื่อ endpoint

--- app/api/endpoints/jobs.py
+++ app/api/endpoints/jobs.py
@@ -8,7 +8,7 @@ from app.services.job import job_service
 
 router = APIRouter()
 
-@router.post("/update_status", status_code=210)
+@router.post("/update_status/", status_code=210)
 def update_status_job(payload:  JobStatusPayload, current_worker: int = Depends(worker_service.verify_token)):
     success = job_service.update_job_status(payload.job_id, payload.status)
     return success
