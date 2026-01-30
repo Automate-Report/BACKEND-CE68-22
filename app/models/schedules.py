173595e2ea345da657f2595b0d1ed1d349823923
Herosuit3 <teerawat2441@gmail.com>
2026-01-30 17:28:50 +0700

    updated db table

--- app/models/schedules.py
+++ app/models/schedules.py
@@ -13,11 +13,9 @@ class Schedule(Base):
     id:Mapped[int] = mapped_column(sa.Integer, autoincrement=True, primary_key=True)#=======================ULID
     project_id:Mapped[int] = mapped_column(sa.ForeignKey("projects.id")) #======================================FK ULID
     asset_id:Mapped[int] = mapped_column(sa.ForeignKey("assets.id")) #======================================FK ULID
-    worker_id:Mapped[int] = mapped_column(sa.ForeignKey("workers.id")) #======================================FK ULID
     cron_expression:Mapped[str] = mapped_column(sa.String(255))
     attack_type:Mapped[ScheduleAttackType] = mapped_column(sa.Enum(ScheduleAttackType))
     is_active:Mapped[bool] = mapped_column(sa.Boolean, default=False)
-    next_run_at:Mapped[datetime.datetime] = mapped_column(sa.DateTime(timezone=True))
     start_date:Mapped[datetime.datetime] = mapped_column(sa.DateTime(timezone=True))
     end_date:Mapped[datetime.datetime] = mapped_column(sa.DateTime(timezone=True))
     created_at:Mapped[datetime.datetime] = mapped_column(sa.DateTime(timezone=True), server_default=sa.sql.func.now())
--- app/models/tags.py
+++ app/models/tags.py
@@ -4,11 +4,31 @@ from typing import Optional
 from sqlalchemy.orm import Mapped, mapped_column
 from app.core.db import Base
 
+class HexColor(sa.TypeDecorator):
+    """Converts hex strings to lowercase and ensures '#' prefix."""
+    
+    impl = sa.String(7)  # Stores as VARCHAR(7)
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return None
+        # Ensure it starts with # and is 7 chars long
+        if not value.startswith('#'):
+            value = f"#{value}"
+        return value.lower()
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return None
+        return value.upper() # Return as uppercase for Python consistency
+
 class Tag(Base):
     __tablename__ = "tags"
     id:Mapped[int] = mapped_column(sa.Integer, autoincrement=True, primary_key=True)#====================ULID
     user_email: Mapped[str] = mapped_column(sa.ForeignKey("users.email"))
     name:Mapped[str] = mapped_column(sa.String(255))
-    description:Mapped[Optional[str]] =  mapped_column(sa.Text, default=None)
+    text_color:Mapped[str] = mapped_column(HexColor)
+    bg_color:Mapped[str] = mapped_column(HexColor)
     created_at:Mapped[datetime.datetime] = mapped_column(sa.DateTime(timezone=True), server_default=sa.sql.func.now())
     updated_at:Mapped[datetime.datetime] = mapped_column(sa.DateTime(timezone=True), server_default=sa.sql.func.now(), onupdate=sa.sql.func.now())
