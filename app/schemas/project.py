decc4d86814fc0d488aa0f4083ffbb2aac54f864
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-02-19 21:20:44 +0700

    เพิ่มข้อมูลในส่วนของ tag

--- app/schemas/project.py
+++ app/schemas/project.py
@@ -18,6 +18,11 @@ class ProjectResponse(BaseModel):
     class Config:
         orm_mode = True # เพื่อให้ Pydantic อ่านข้อมูลจาก ORM objects ได้ ไว้ใช้กับ SQLAlchemy ตอนทำ database
 
+class Tag(BaseModel):
+    name: str
+    text_color: str
+    bg_color: str
+
 class ProjectSummaryResponese(BaseModel):
     id: int
     name: str
@@ -25,6 +30,7 @@ class ProjectSummaryResponese(BaseModel):
     role: str = "owner"
     assets_cnt: int
     vuln_cnt: int
+    tags: List[Tag] = None
     created_at: datetime
     updated_at: datetime
 
