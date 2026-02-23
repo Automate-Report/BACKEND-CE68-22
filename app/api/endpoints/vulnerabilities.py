b7a3584b950f3344a53eac0db3c6f095a7afc55f
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-02-23 17:49:25 +0700

    เพิ่ม endpoint cnt status

--- app/api/endpoints/vulnerabilities.py
+++ app/api/endpoints/vulnerabilities.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException, Query
 from typing import List, Optional
 
+from app.schemas.vulnerability import SummaryCntVlun
+
 from app.services.vulnerability import vuln_service
 from app.services.asset import asset_service
 
@@ -8,7 +10,7 @@ router = APIRouter()
 
 
 # GET cnt of vuln using project_id
-@router.get("cnt/{project_id}")
+@router.get("/cnt/{project_id}")
 async def get_cnt_vulns_by_project_id(project_id: int):
     asset_ids = asset_service.get_asset_ids_by_project_id(project_id)
 
@@ -21,3 +23,36 @@ async def get_cnt_vulns_by_project_id(project_id: int):
     return {
         "vuln_cnt": cnt
     }
+
+@router.get("/summary/vuln/{project_id}", response_model=SummaryCntVlun)
+async def get_summary_vuln_by_project_id(project_id: int):
+    asset_ids = asset_service.get_asset_ids_by_project_id(project_id)
+
+    if not asset_ids:
+        return SummaryCntVlun(
+            total=0,
+            open_cnt=0,
+            tp_cnt=0,
+            in_progress_cnt=0,
+            fixed_cnt=0
+        )
+    
+    result = SummaryCntVlun(
+        total=0,
+        open_cnt=0,
+        tp_cnt=0,
+        in_progress_cnt=0,
+        fixed_cnt=0
+    )
+    
+    for id in asset_ids:
+        value = vuln_service.cnt_status_vuln_by_asset_id(id)
+
+        result.total+=value.total
+        result.open_cnt+=value.open_cnt
+        result.tp_cnt+=value.tp_cnt
+        result.in_progress_cnt+=value.in_progress_cnt
+        result.fixed_cnt+=value.fixed_cnt
+
+    return result
+    
