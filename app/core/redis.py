7adb85d21408a8e2279cd751eb23f9125af382cf
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-03-01 21:10:09 +0700

    clean comment

--- app/core/redis.py
+++ app/core/redis.py
@@ -3,39 +3,16 @@ from config import settings
 
 pool_blacklist = redis.ConnectionPool.from_url(
     url=settings.BACKLIST_REDIS_URL,
-    # url="redis://localhost:5678/0",
     decode_responses=True
 )
 
 pool_jobs = redis.ConnectionPool.from_url(
     url=settings.JOBS_REDIS_URL,
-    # url="redis://localhost:5678/1",
     decode_responses=True
 )
 
-
-# redis_client = redis.Redis(
-#     host="10.20.20.108",
-#     port=5678,
-#     db=0,
-#     decode_responses=True
-# )
-# redis_worker = redis.Redis(
-#     host="10.20.20.108",
-#     port=5678,
-#     db=1,
-#     decode_responses=True
-# )
 
 redis_client = redis.Redis(connection_pool=pool_blacklist)
 redis_jobs = redis.Redis(connection_pool=pool_jobs)
 
 QUEUE_KEY = "system:queue:work" #เก็บงานที่พร้อมทำ
-
-# For localhost user
-
-# docker pull redis:7
-# docker run -d ^
-#   --name redis-server ^
-#   -p 5678:6379 ^
-#   redis:7
