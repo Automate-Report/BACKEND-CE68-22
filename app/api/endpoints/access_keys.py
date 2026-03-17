6434453d613de125d45cc07ea12eb4e6572f4535
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-03-17 14:52:16 +0700

    แก้ endpoint ของdelete access key

--- app/api/endpoints/access_keys.py
+++ app/api/endpoints/access_keys.py
@@ -36,21 +36,9 @@ async def get_access_key_by_id(
         
     return access_key
 
-@router.get("/byWorkerId/{worker_id}", response_model=AccssKeyResponse)
-async def get_access_key_by_worker_id(worker_id: int):
-    # เรียก Service เพื่อดึงข้อมูลตาม ID
-    fake_current_user_id = 1
-    worker = access_key_service.get_access_key_by_worker_id(worker_id)
-
-    if not worker:
-        from fastapi import HTTPException
-        raise HTTPException(status_code=404, detail="Not found")
-        
-    return worker
-
 @router.delete("/{access_key_id}")
-async def delete_access_key(access_key_id: int):
-    success = access_key_service.delete_access_key_by_id(access_key_id)
+async def delete_access_key(access_key_id: int, db: AsyncSession = Depends(get_db)):
+    success = await access_key_service.delete_access_key_by_id(access_key_id, db)
 
     if not success:
         raise HTTPException(status_code=404, detail="Access Key not found")
