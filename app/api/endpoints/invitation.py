cdfa180a871e1b0bcce69d978bf475c790171b98
Herosuit3 <teerawat2441@gmail.com>
2026-03-18 17:48:39 +0700

    update /invitation/all

--- app/api/endpoints/invitation.py
+++ app/api/endpoints/invitation.py
@@ -5,20 +5,39 @@ from typing import Optional, List
 
 from app.deps.auth import get_current_user
 
-from app.schemas.project import ProjectMemberResponse
+from app.schemas.invite import InvitationResponse
 
 from app.services.project_member import project_member_service
+from app.services.project import project_service
+from app.services.userauthen import userauthen_service
 
 router = APIRouter()
 
-@router.get("/all", response_model=List[ProjectMemberResponse])
+@router.get("/all", response_model=List[InvitationResponse])
 def get_all_invitations(user = Depends(get_current_user)):
+
+    return_result =[]
 
     result = project_member_service.get_invitations_by_user_id(
         user_id=user["sub"]
         )
     
-    return result
+    for invite in result:
+        project = project_service.get_project_by_id(invite["project_id"])
+        if project:
+            owner_info = userauthen_service.get_user_by_id(project["email"])
+            invite_response = InvitationResponse(
+                project_id=invite["project_id"],
+                email=invite["email"],
+                project_name=project["name"],
+                project_owner=f"{owner_info['firstname']} {owner_info['lastname']}",
+                role=invite["role"],
+                status=invite["status"],
+                invited_at=invite["invited_at"]
+            )
+            return_result.append(invite_response)
+    
+    return return_result
 
 @router.put("/accept/{project_id}")
 def accept_invitation(project_id: int, user = Depends(get_current_user)):
