5ce4d8d4e2a7b8e6879ac5c1d707731ca072dcae
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-03-23 19:46:43 +0700

    ปรับ vulnlib table

--- app/models/vuln_libs.py
+++ app/models/vuln_libs.py
@@ -10,7 +10,7 @@ class VulnLib(Base):
     __tablename__ = "vuln_libs"
     id:Mapped[int] = mapped_column(sa.Integer, autoincrement=True, primary_key=True)#=======================ULID
     vuln_type:Mapped[str] = mapped_column(sa.ARRAY(sa.String))
-    sub_types:Mapped[List[str]] = mapped_column(sa.VARCHAR(255))
+    sub_types:Mapped[List[str]] = mapped_column(sa.ARRAY(sa.String), nullable=False)
     cvss_score:Mapped[float] = mapped_column(sa.FLOAT)
     cvss_vector:Mapped[str] = mapped_column(sa.VARCHAR(255))
     severity:Mapped[str] = mapped_column(sa.VARCHAR(255))
