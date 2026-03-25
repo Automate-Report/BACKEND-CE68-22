c99aed815c7313e0f0733b5a473e1e4d9e56f9b4
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-03-25 19:29:07 +0700

    ปรับ code

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -1,13 +1,29 @@
 from fastapi import APIRouter, HTTPException, Query, Depends
 from typing import List, Optional
 
+from app.core.db import get_db
+from app.deps.auth import get_current_user
+from app.deps.role import get_current_project_role
+
 from app.services.user import user_service 
+from sqlalchemy.ext.asyncio import AsyncSession
+
 
 router = APIRouter()
 
 # GET check if user exist
 @router.get("/check")
-async def check_exist(email: str):
-
-    result = user_service.is_user_exist(email)
+async def check_exist(
+    email: str,
+    user = Depends(get_current_user),
+    role = Depends(get_current_project_role),
+    db: AsyncSession = Depends(get_db)
+):
+    if role != "owner":
+        raise HTTPException(status_code=403, detail="Not Authorizations")
+    
+    result = await user_service.is_user_exist(
+        email=email,
+        db=db
+    )
     return result
--- app/services/user.py
+++ app/services/user.py
@@ -1,14 +1,25 @@
+import sqlalchemy as sa
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.users import User 
 
 class UserService:
 
-    def is_user_exist(self,email:str):
-        users = self._read_json()
-
-        for user in users:
-            if user["email"] == email:
-                return True
+    async def is_user_exist(self, email:str, db: AsyncSession):
+        """
+        ตรวจสอบว่ามี User อีเมลนี้อยู่ในระบบหรือไม่ (SQL Version)
+        """
+        # 1. สร้าง Query เพื่อเช็คว่ามีแถวที่ email ตรงกันไหม
+        # ใช้ sa.exists() จะมีประสิทธิภาพสูงที่สุด (สเกลได้ดีกว่าดึงข้อมูลมาทั้งก้อน)
+        query = sa.select(sa.exists().where(User.email == email))
         
-        return False
+        try:
+            # 2. Execute และดึงค่า Boolean ออกมา
+            result = await db.execute(query)
+            return result.scalar() # คืนค่า True ถ้าเจอ, False ถ้าไม่เจอ
+
+        except Exception as e:
+            print(f"❌ Error checking user existence: {e}")
+            return False
 
 
 # สร้าง Instance ไว้ให้ Router เรียกใช้
