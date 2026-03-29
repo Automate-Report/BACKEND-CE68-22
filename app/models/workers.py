5d61fc66d8e6768d50ea9d74dec7e171918de5ab
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-03-29 19:17:43 +0700

    แก้ FK sccess key id ให้ null ได้

--- app/models/workers.py
+++ app/models/workers.py
@@ -17,7 +17,7 @@ class Worker(Base):
     __tablename__ = "workers"
     id:Mapped[int] = mapped_column(sa.Integer, autoincrement=True, primary_key=True)#=======================ULID
     project_id:Mapped[int] = mapped_column(sa.ForeignKey("projects.id"))
-    access_key_id:Mapped[int] = mapped_column(sa.ForeignKey("access_keys.id"))
+    access_key_id:Mapped[int] = mapped_column(sa.ForeignKey("access_keys.id", ondelete="SET NULL"), nullable=True)
     owner:Mapped[Optional[str]] = mapped_column(sa.ForeignKey("users.email"), nullable=True, default=None)
     thread_number:Mapped[int] = mapped_column(sa.Integer, default=1)
     current_load:Mapped[int] = mapped_column(sa.Integer, default=0)
